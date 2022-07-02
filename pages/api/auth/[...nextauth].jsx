786601f3647950ddc83bccb3d32ffc92938602d2
AdityaKirad <kiradaditya2001@gmail.com>
2022-07-03 01:42:20 +0530

    connected to mysql database to store user token for later use

---
+++ pages/api/auth/[...nextauth].jsx
@@ -0,0 +1,142 @@
+import NextAuth from 'next-auth/next';
+import DiscordProvider from 'next-auth/providers/discord';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const DISCORD_AUTHORIZATION_URL = "https://discord.com/api/oauth2/authorize" + 
+    new URLSearchParams({
+        prompt: "consent",
+        access_type: "offline",
+        response_type: "code",
+    })
+
+async function refreshAccessToken(token) {
+    try {
+        const url = "https://discord.com/api/oauth2/token" +
+        new URLSearchParams({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+            grant_type: "refresh_token",
+            refresh_token: token.refreshToken,
+        })
+
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            method: "POST",
+        })
+
+        const refreshedTokens = await response.json()
+
+        if (!response.ok) {
+            throw refreshedTokens
+        }
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+            refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+        }
+    } catch ( error ) {
+        console.log( error )
+
+        return {
+            ...token,
+            error: "RefreshAccessTokenError",
+        }
+    }
+}
+
+async function updateTokeninDatabase(discordId, discordName, accessToken, refreshToken ){
+    try {
+        const existingUser = await prisma.userswebinformation.findUnique({
+            where: {
+                discordId: Number(discordId),
+            },
+        });
+        if(existingUser) {
+            const existingUser = await prisma.userswebinformation.update({
+                where: {
+                    discordId: Number(discordId),
+                },
+                data: {
+                    discordId: Number(discordId),
+                    discordName: discordName,
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                }
+            });
+            return existingUser;
+        }
+        else {
+            const newUser = await prisma.userswebinformation.create({
+                data: {
+                    discordId: Number(discordId),
+                    discordName: discordName,
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                },
+            });
+            return newUser;
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+export default NextAuth({
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+            callbackUrl : process.env.NEXTAUTH_URL,
+            token: "https://discord.com/api/oauth2/token",
+            userinfo: "https://discord.com/api/users/@me",
+            authorization: {
+                params: {
+                    scope: 'identify email guilds applications.commands.permissions.update'
+                }
+            },
+        }),
+    ],
+
+    callbacks: {
+        async jwt({ token, user, account }) {
+            if ( account && user ) {
+                return {
+                    accessToken: account.access_token,
+                    accessTokenExpires: Date.now() + account.expires_at * 1000,
+                    refreshToken: account.refresh_token,
+                    user,
+                }
+            }
+
+            if ( Date.now() < token.accessTokenExpires ) {
+                return token
+            }
+
+            return refreshAccessToken(token)
+        },
+
+        async session({ session, token, accessToken, refreshToken, discordId, discordName }) {
+            session.user = token.user
+            accessToken = token.accessToken
+            refreshToken = token.refreshToken
+            session.error = token.error
+            discordId = token.user.id
+            discordName = token.user.name
+            updateTokeninDatabase(discordId, discordName, accessToken, refreshToken)
+            .catch((e) => {
+                throw e
+            })
+            .finally(async () => {
+                await prisma.$disconnect()
+            })
+            return session
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+})
---
+++ pages/index.jsx
@@ -0,0 +1,58 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import NavBar from '../components/NavBar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Grow from '@mui/material/Grow';
+import { FaDiscord } from "react-icons/fa";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material';
+import { signIn, useSession } from 'next-auth/react';
+import '@fontsource/macondo/400.css';
+import styles from '../styles/Home.module.css'; 
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const theme = useTheme();
+  const { data: session, status } = useSession();
+  
+  const router = useRouter();
+  if (status === "authenticated"){
+    router.push("/dashboard")
+  }
+  return (
+    <>
+      <Head>
+        <title>Lucario-The Discord Bot</title>
+        <meta name="description" content="Lucario is a feature-rich and modular discord bot for your Discord server.
+         With web configuration, moderation, anti-spam, auto roles,...." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/Untitled design (2)_auto_x2_colored_toned_light_ai.png" />
+      </Head>
+      <Box className={styles.background}>  
+            <NavBar />
+        <Box className={styles.content}>
+          <Grow in={true} style={{}}{...{ timeout: 3000}}>
+          <Box className={styles.description}>
+            <Box className={styles.about}>
+              <Typography className={styles.dashboard} variant="h3" sx={{ fontFamily: 'Macondo, cursive', color: 'white', fontWeight: 'bold' }}>Lucario Bot DashBoard</Typography>
+              <Typography className={styles.shortDescription}variant="h5" sx={{ fontFamily: 'Macondo, cursive', color: 'white' }}>A fully customizable server moderation Discord bot for
+               your Discord server that features a simple and intuitive
+                web dashboard. Server management just got a whole lot
+                 easier.</Typography>
+            </Box>
+            <Box className={styles.buttons}>
+              <Button className={styles.addToDiscord} color="inherit"
+              href='https://discord.com/api/oauth2/authorize?client_id=961965367767470171&permissions=8&scope=bot%20applications.commands'
+              target='_blank'>Add To Discord</Button>
+              <Button className={styles.loginWithDiscord} color="inherit" onClick={() => signIn('discord')}>Login <FaDiscord style={{ width: "1.5em", height: "1.5em", marginLeft: "0.5vw" }}/></Button>
+            </Box>
+          </Box>
+          </Grow>
+        </Box>
+      </Box>
+      
+    </>
+  )
+}
